8f583b0207ec826eb36a629f44ee73eccd48e176
tnam2132 <tnam2132@gmail.com>
2023-02-12 22:54:39 +0700

    add lại nhé ^_^

--- src/main/java/com/devz/hotelmanagement/rest/controllers/RoomRestController.java
+++ src/main/java/com/devz/hotelmanagement/rest/controllers/RoomRestController.java
@@ -1,31 +1,31 @@
-//package com.devz.hotelmanagement.rest.controllers;
-//
-//import com.devz.hotelmanagement.entities.Room;
-//import com.devz.hotelmanagement.services.RoomService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@CrossOrigin("*")
-//@RestController
-//@RequestMapping("/api/rooms")
-//public class RoomRestController {
-//    @Autowired
-//    RoomService roomService;
-//
-//    @GetMapping
-//    public List<Room> getAll() {
-//        return roomService.findAll();
-//    }
-//
-//    @PostMapping
-//    public Room create(@RequestBody Room room) {
-//        return roomService.create(room);
-//    }
-//
-//    @PostMapping
-//    public Room update(@RequestBody Room room) {
-//        return roomService.update(room);
-//    }
-//}
+package com.devz.hotelmanagement.rest.controllers;
+
+import com.devz.hotelmanagement.entities.Room;
+import com.devz.hotelmanagement.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomRestController {
+    @Autowired
+    RoomService roomService;
+
+    @GetMapping
+    public List<Room> getAll() {
+        return roomService.findAll();
+    }
+
+    @PostMapping
+    public Room create(@RequestBody Room room) {
+        return roomService.create(room);
+    }
+
+    @PostMapping
+    public Room update(@RequestBody Room room) {
+        return roomService.update(room);
+    }
+}
