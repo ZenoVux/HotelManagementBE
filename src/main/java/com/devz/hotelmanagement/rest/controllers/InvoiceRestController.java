ce540e0aef816c9dab359b40df29cc86662e5a09
vietvp892002 <vietvp892002@gmail.com>
2023-02-12 13:05:10 +0700

    RestController

---
+++ src/main/java/com/devz/hotelmanagement/rest/controllers/InvoiceRestController.java
@@ -0,0 +1,38 @@
+package com.devz.hotelmanagement.rest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devz.hotelmanagement.entities.Invoice;
+import com.devz.hotelmanagement.services.InvoiceService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/invoices")
+public class InvoiceRestController {
+	@Autowired
+    private InvoiceService invoiceService;
+
+    @GetMapping
+    public List<Invoice> getAll() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping
+    public Invoice create(@RequestBody Invoice invoice) {
+        return invoiceService.create(invoice);
+    }
+
+    @PutMapping
+    public Invoice update(@RequestBody Invoice invoice) {
+        return invoiceService.update(invoice);
+    }
+}
