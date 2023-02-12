4516832dc70dcae84df618f31ae0e8e7e8c49fb6
hungNumNum <jimmyhunganh2k@gmail.com>
2023-02-12 22:33:08 +0700

    hung_5rest

---
+++ src/main/java/com/devz/hotelmanagement/rest/controllers/BedTypeRestController.java
@@ -0,0 +1,32 @@
+package com.devz.hotelmanagement.rest.controllers;
+
+import com.devz.hotelmanagement.entities.Account;
+import com.devz.hotelmanagement.entities.BedType;
+import com.devz.hotelmanagement.services.BedTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/bedtypes")
+public class BedTypeRestController {
+    @Autowired
+    BedTypeService bedTypeService ;
+    @GetMapping
+    public List<BedType> getAll() {
+        return bedTypeService.findAll();
+    }
+
+    @PostMapping
+    public BedType create(@RequestBody BedType bedType) {
+        return bedTypeService.create(bedType);
+    }
+
+    @PutMapping
+    public BedType update(@RequestBody BedType bedType) {
+        return bedTypeService.update(bedType);
+    }
+
+}
