8d812489acd0a1b4c72224212913eb61c5b0dbf1
vietvp892002 <vietvp892002@gmail.com>
2023-02-22 21:47:28 +0700

    v

--- src/main/java/com/devz/hotelmanagement/rest/controllers/PaymentMethodRestController.java
+++ src/main/java/com/devz/hotelmanagement/rest/controllers/PaymentMethodRestController.java
@@ -1,5 +1,6 @@
 package com.devz.hotelmanagement.rest.controllers;
 
+import com.devz.hotelmanagement.entities.Customer;
 import com.devz.hotelmanagement.entities.PaymentMethod;
 import com.devz.hotelmanagement.services.PaymentMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,11 @@ public class PaymentMethodRestController {
     public List<PaymentMethod> getAll() {
         return paymentMethodService.findAll();
     }
-
+    @GetMapping("/{id}")
+    public PaymentMethod getCustomerById(@PathVariable int id) {
+        PaymentMethod paymentMethod = paymentMethodService.findById(id);
+        return paymentMethod;
+    }
     @PostMapping
     public PaymentMethod create(@RequestBody PaymentMethod paymentMethod) {
         return paymentMethodService.create(paymentMethod);
