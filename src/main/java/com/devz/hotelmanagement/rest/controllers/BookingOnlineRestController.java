d476d94b678b416e38194d026b3d9687c330ba1f
hungNumNum <jimmyhunganh2k@gmail.com>
2023-04-19 19:59:40 +0700

    hung 19/4

--- src/main/java/com/devz/hotelmanagement/rest/controllers/BookingOnlineRestController.java
+++ src/main/java/com/devz/hotelmanagement/rest/controllers/BookingOnlineRestController.java
@@ -1,106 +1,69 @@
 package com.devz.hotelmanagement.rest.controllers;
 
-import com.devz.hotelmanagement.entities.Booking;
 import com.devz.hotelmanagement.entities.Promotion;
 import com.devz.hotelmanagement.entities.Room;
+import com.devz.hotelmanagement.entities.RoomImage;
 import com.devz.hotelmanagement.entities.RoomType;
-import com.devz.hotelmanagement.models.BookingOnlReq;
-import com.devz.hotelmanagement.models.RoomBooking;
-import com.devz.hotelmanagement.services.BookingService;
+import com.devz.hotelmanagement.entities.ServiceRoom;
 import com.devz.hotelmanagement.services.PromotionService;
+import com.devz.hotelmanagement.services.RoomImageService;
 import com.devz.hotelmanagement.services.RoomService;
 import com.devz.hotelmanagement.services.RoomTypeService;
+import com.devz.hotelmanagement.services.ServiceRoomService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/booking-online")
 public class BookingOnlineRestController {
 
-    @Autowired
-    private BookingService bookingService;
+	@Autowired
+	private ServiceRoomService serviceRoomService;
 
-    @Autowired
-    private RoomService roomService;
+	@Autowired
+	private RoomImageService roomImageService;
 
-    @Autowired
+	@Autowired
+	private RoomTypeService roomTypeService;
+
+	@Autowired
+	private RoomService roomService;
+	
+	@Autowired
     private PromotionService promotionService;
 
-    @Autowired
-    private RoomTypeService roomTypeService;
+	@GetMapping("/room-types")
+	public List<RoomType> findAllRoomType() {
+		return roomTypeService.findAll();
+	}
 
-    @GetMapping("/room-types")
-    public List<RoomType> findAllRoomType() {
-        return roomTypeService.findAll();
-    }
+	@GetMapping("/services")
+	public List<ServiceRoom> findAllService() {
+		return serviceRoomService.findAll();
+	}
+	
+	@GetMapping("/promotions")
+	public List<Promotion> findAllPomotion() {
+		return promotionService.findAll();
+	}
 
-    @GetMapping("/room-types/{code}")
-    public RoomType findRoomTypeByCode(@PathVariable("code") String code) {
-        return roomTypeService.findByCode(code);
-    }
+	@GetMapping("/rooms")
+	public List<Room> findAllRooms() {
+		return roomService.findAll();
+	}
 
-    @GetMapping("/info")
-    public List<RoomBooking> getInfoRoomBooking(@RequestParam("checkinDate") String checkin, @RequestParam("checkoutDate") String checkout, @RequestParam("roomType") String roomType) {
+	@GetMapping("/room-types/{code}")
+	public RoomType findRoomTypeByCode(@PathVariable("code") String code) {
+		return roomTypeService.findByCode(code);
+	}
 
-        try {
-
-            if (roomType == "") roomType = null;
-            DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-            Date checkinDate = formatter.parse(checkin);
-            Date checkoutDate = formatter.parse(checkout);
-
-            List<Object[]> infoRoomBooking = bookingService.getInfoRoomBooking(roomType, checkinDate, checkoutDate);
-
-            return infoRoomBooking.stream().map(bookingInfo -> {
-                RoomBooking roomBooking = new RoomBooking();
-                roomBooking.setRoomType(roomTypeService.getRoomTypeByCode((String) bookingInfo[2]));
-                roomBooking.setName((String) bookingInfo[0]);
-                roomBooking.setQuantity((Long) bookingInfo[1]);
-
-                List<Promotion> promotions = promotionService.findByRoomType((String) bookingInfo[2]);
-
-                if (promotions != null && !promotions.isEmpty()) {
-                    roomBooking.setPromotion(promotions.get(0));
-                } else {
-                    roomBooking.setPromotion(null);
-                }
-
-                List<Integer> roomIds = bookingService.getRoomsByTimeBooking((String) bookingInfo[0], checkinDate, checkoutDate);
-                List<Room> roomList = roomService.findByIds(roomIds);
-                roomBooking.setListRooms(roomList);
-
-                DoubleSummaryStatistics priceStats = roomList.stream().map(Room::getPrice).filter(Objects::nonNull).mapToDouble(Double::doubleValue).summaryStatistics();
-                roomBooking.setMinPrice(priceStats.getMin());
-                roomBooking.setMaxPrice(priceStats.getMax());
-
-                roomBooking.setMaxAdults((Long) bookingInfo[3]);
-                roomBooking.setMaxChilds((Long) bookingInfo[4]);
-
-                return roomBooking;
-            }).collect(Collectors.toList());
-
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @PostMapping("/get-booking")
-    public Booking createBooking(@RequestBody BookingOnlReq bookingOnlReq){
-
-        Booking booking = new Booking();
-        return booking;
-
-    }
+	@GetMapping("/img/{codeRoom}")
+	public List<RoomImage> getByCodeRoom(@PathVariable("codeRoom") String codeRoom) {
+		return roomImageService.getListByCodeRoom(codeRoom);
+	}
 
 }
--- src/main/resources/static/assets/js/booking.js
+++ src/main/resources/static/assets/js/booking.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -52,33 +54,57 @@ app.config(function ($routeProvider) {
             templateUrl: "/assets/view/home.html",
             controller: "bookOnlineCtrl",
         })
-        .when("/book-now", {
-            templateUrl: "/assets/view/book.html",
-            controller: "bookCtrl",
-        })
         .when("/book/:code", {
             templateUrl: "/assets/view/book.html",
             controller: "bookCtrl",
         })
-        .otherwise({
-            redirectTo: "/"
-        });
-});
+})
+
 
 app.controller("bookOnlineCtrl", function ($scope, $http, $location) {
     $scope.form = {}
+    $scope.init = function () {
+        $http.get("http://localhost:8000/api/booking-online/room-types").then(resp => {
+            $scope.roomTypes = resp.data;
+            // console.log($scope.roomTypes)
+        }).catch(error => {
+            console.log("Error", error);
+        })
+        $http.get("http://localhost:8000/api/booking-online/rooms").then(resp => {
+            $scope.rooms = resp.data;
+            // console.log($scope.rooms)
+        }).catch(error => {
+            console.log("Error", error);
+        })
+        $http.get("http://localhost:8000/api/booking-online/services").then(resp => {
+            $scope.services = resp.data;
+            // console.log($scope.services)
+        }).catch(error => {
+            console.log("Error", error);
+        })
 
-    $scope.bookNow = function () {
-        $location.path("/book-now");
+        $http.get("http://localhost:8000/api/booking-online/promotions").then(resp => {
+            $scope.promotions = resp.data;
+            // console.log($scope.promotions)
+        }).catch(error => {
+            console.log("Error", error);
+        })
+
     }
 
-    $scope.getRoomType = function () {
-        $http.get("/api/booking-online/room-types").then(resp => {
-            $scope.roomTypes = resp.data;
+
+    $scope.loadImageRoom = function (codeRoom) { // load anh phong
+        $http.get("http://localhost:8000/api/booking-online/img/" + codeRoom).then(resp => {
+            $scope.imageRooms = resp.data;
+            // console.log(resp.data)
         }).catch(error => {
             console.log("Error", error);
         })
     }
+
+    $scope.url = function(imageName){
+        return `http://localhost:8000/images/${imageName}`;
+}
 
     $scope.close = function () {
         var myModalEl = document.getElementById('roomModal1');
@@ -86,71 +112,58 @@ app.controller("bookOnlineCtrl", function ($scope, $http, $location) {
         modal.hide();
     }
 
-    $scope.views = function (roomType, link) {
-        $scope.link = link;
+    $scope.views = function (roomType, code) {
+        $scope.loadImageRoom(code)
+
+        // $scope.link = link;
         $scope.form = roomType;
     }
-
     $scope.books = function (roomTypeCode) {
-        $scope.close();
+        // $scope.close();
         $location.path("/book/" + roomTypeCode);
-    }
-
-    $scope.init = function () {
-        $scope.getRoomType();
     }
 
     $scope.init();
 });
 
-app.controller("bookCtrl", function ($scope, $http, $routeParams, $filter) {
+app.controller("bookCtrl", function ($scope, $http, $routeParams) {
+    $scope.formBook={};
+    var code = $routeParams.code
 
-    $scope.formBook = {};
-    $scope.formBook.numAdults = 2;
-    $scope.formBook.numChildren = 0;
-    $scope.formBook.numRooms = 1;
-    $scope.infoRoomBooking = [];
-    $scope.roomTypes = [];
-
-    // var code = $routeParams.code
-    var code = null
-
-    $scope.getRoomType = function () {
-        $http.get("/api/booking-online/room-types").then(resp => {
-            $scope.roomTypes = resp.data
-        }).catch(error => {
-            console.log("Error", error);
-        });
+    $scope.init = function(){
+    $scope.checkin=document.getElementById("myDate").value;
+    $scope.checkout=document.getElementById("myDate1").value;
+    $scope.formBook ={
+        checkin:$scope.checkin,
+        checkout:$scope.checkout,
+    }
     }
 
-    $scope.changeDate = function () {
-        $scope.infoRoomBooking = [];
-    }
 
-    $scope.closeDropdown = function () {
-        $('.dropdown-menu').removeClass('show');
-    };
+    
+    $http.get("http://localhost:8000/api/booking-online/room-types/" + code).then(resp => {
+        $scope.form = resp.data
+    }).catch(error => {
+        console.log("Error", error);
+    })
 
-    $scope.checkRoom = function () {
-        if ($scope.formBook.roomType == null || $scope.formBook.roomType == undefined) {
-            $scope.formBook.roomType = "";
-        };
-        $http.get('/api/booking-online/info', {
-            params: {
-                checkinDate: $filter('date')($scope.formBook.checkinDate, 'dd-MM-yyyy'),
-                checkoutDate: $filter('date')($scope.formBook.checkoutDate, 'dd-MM-yyyy'),
-                roomType: $scope.formBook.roomType
-            }
-        }).then(function (response) {
-            $scope.infoRoomBooking = response.data;
-            console.log($scope.infoRoomBooking);
-        }).catch(function (error) {
-            console.error('Error fetching data:', error);
-        });
-    }
+
+    paypal.Buttons({ // vi du phuong thuc thanh toan
+        style: {
+            label: 'checkout',
+            size: 'medium', // small | medium | large | responsive
+            shape: 'pill', // pill | rect
+            color: 'gold', // gold | blue | silver | black,
+            layout: 'vertical'
+        },
+
+    }).render('#paypal-button-container')
+
+
+
 
     $scope.countDown = function () {
-
+        alert("Hay chon ngay checkin-out de kiem tra lương phòng còn lại")
         var countdownElement = document.getElementById('countdown');
 
         var startTime = new Date().getTime();
@@ -174,40 +187,11 @@ app.controller("bookCtrl", function ($scope, $http, $routeParams, $filter) {
 
     }
 
-    $scope.openModalBooking = function () {
-        // if ($scope.infoRoomBooking == null || $scope.infoRoomBooking.length == 0) {
-        //     alert("Vui lòng chọn ngày checkin, checkout và kiểm tra lại lượng phòng còn lại!");
-        //     return;
-        // }
-        // if ($scope.infoRoomBooking[0].quantity == 0) {
-        //     alert("Phòng đã hết!");
-        //     return;
-        // }
-        // if ($scope.infoRoomBooking[0].quantity < $scope.formBook.numRooms) {
-        //     var r = confirm("Số lượng phòng còn lại không đủ, bạn có muốn đặt phòng không?"); đổi sang đặt số phòng = số phòng còn lại
-        //     if (r == false) {
-        //         return;
-        //     }
-        // }
-        $scope.countDown();
-        $('#book-modal').modal('show');
+    $scope.submit = function () {
+        alert("check đầu ra ở console.log")
+        console.log($scope.formBook)
     }
 
-    $scope.getBooking = function () {
-        console.log($scope.formBook);
-        // $http.post("/api/booking-online/get-booking", $scope.formBook).then(resp => {
-        //     console.log(resp.data);
-        //     alert("Đặt phòng thành công!");
-        //     //window.location.href = "/booking-online";
-        // }).catch(error => {
-        //     console.log("Error", error);
-        // });
-    }
-
-    $scope.init = function () {
-        $scope.getRoomType();
-    }
-
-    $scope.init()
+$scope.init()
 
 });
